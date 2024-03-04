e914d50184f3b3d92421db793c36adab4d7da93d
plh00004 <plh00004@red.ujaen.es>
2024-03-04 16:34:29 +0100

    Actualización a flask

--- app.py
+++ app.py
@@ -1,11 +1,7 @@
 from flask import Flask, render_template, request, jsonify
-from flask_mysqldb import MySQL
 from flask_cors import CORS
-from flask import send_from_directory
-import os
 from faunadb import query as q
 from faunadb.client import FaunaClient
-
 from Models.sensor import Sensor
 
 fauna_client = FaunaClient(secret="fnAE-czWhAAAzJpuciEYP-P-UZlSDIp4blaPP4KT")
@@ -13,38 +9,24 @@ fauna_client = FaunaClient(secret="fnAE-czWhAAAzJpuciEYP-P-UZlSDIp4blaPP4KT")
 app = Flask(__name__)
 CORS(app)
 
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = '12345'
-app.config['MYSQL_DB'] = 'SensorizedApiWeb'
-mysql = MySQL(app)
 
 sensor = ""
 
 
-#Ruta base: página principal
+# Ruta base: página principal
 @app.route('/', methods=['GET', 'POST'])
-def index() :
+def index():
     return render_template('index.html')
 
 
-#Ruta para visualizar los datos
+# Ruta para visualizar los datos
 @app.route('/visualizar_datos', methods=['GET', 'POST'])
-def elegir_futuro() :
+def elegir_futuro():
     global sensor
     if request.method == 'POST':
         sensor = request.form['sensor']
         return render_template('visualizar_datos.html', sensor=sensor)
 
-
-@app.route('/obtener_datos', methods=['GET'])
-def obtener_datos():
-    global sensor
-    cur = mysql.connection.cursor()
-    cur.execute('SELECT * FROM ' + sensor + ' ORDER BY id DESC LIMIT')
-    data = cur.fetchall()
-    cur.close()
-    return jsonify(data)
 
 @app.route('/baterias', methods=['POST','GET'])
 async def datosBateria():
